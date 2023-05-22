8014dbe276f6b76c4e469629297aac688eb15850
Hector Rojas <hrpxa270@hotmail.com>
2023-05-21 18:46:51 -0600

    first commit

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -31,13 +31,13 @@ export default NextAuth({
   secret: process.env.NEXTAUTH_SECRET
 });
 
-const SignInUser = async ({ password, user }) => {
-  if (!user.password) {
-    throw new Error("Please enter your password.");
-  }
-  const testPassword = await bcrypt.compare(password, user.password);
-  if (!testPassword) {
-    throw new Error("Email or password is wrong!");
-  }
-  return user;
-};
+// const SignInUser = async ({ password, user }) => {
+//   if (!user.password) {
+//     throw new Error("Please enter your password.");
+//   }
+//   const testPassword = await bcrypt.compare(password, user.password);
+//   if (!testPassword) {
+//     throw new Error("Email or password is wrong!");
+//   }
+//   return user;
+// };
