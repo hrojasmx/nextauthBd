ecf6a568c1a66e3759f7e7e1651b86120e32d3ed
Hector Rojas <hrpxa270@hotmail.com>
2023-05-22 13:34:01 -0600

    mongodbAdaptor

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,7 +6,13 @@ import DiscordProvider from "next-auth/providers/discord";
 import TwitterProvider from "next-auth/providers/twitter";
 import Auth0Provider from "next-auth/providers/auth0";
 
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
+
+
 export default NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
+
   providers: [
     FacebookProvider({
       clientId: process.env.FACEBOOK_ID as string,
